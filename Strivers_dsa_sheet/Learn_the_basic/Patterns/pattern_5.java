4199ae2fbba775b3d3e00e19c02d8e759ca9879d
Rutuja Upase <rutujaupase13@gmail.com>
2026-06-17 10:07:23 +0530

    Add solution for pattern 5

---
+++ Strivers_dsa_sheet/Learn_the_basic/Patterns/pattern_5.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class Main
+{
+	public static void main(String[] args) {
+	    Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the value: ");
+		int num = sc.nextInt();
+		for(int i = num-1;i >= 0;i--)
+		{
+		    for(int j = 0; j <= i;j++)
+		    {
+		        System.out.print("*");
+		    }
+		    System.out.println();
+		}
+		
+	}
+}
