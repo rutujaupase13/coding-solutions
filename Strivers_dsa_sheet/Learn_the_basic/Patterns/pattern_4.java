90aed2ab73ac1f81d8d06a25999fd5c30d32e643
Rutuja Upase <rutujaupase13@gmail.com>
2026-06-17 10:02:30 +0530

    Add solution for pattern 4

---
+++ Strivers_dsa_sheet/Learn_the_basic/Patterns/pattern_4.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class Main
+{
+	public static void main(String[] args) {
+	    Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the value: ");
+		int num = sc.nextInt();
+		int count = 1;
+		for(int i = 0;i < num;i++)
+		{
+		    for(int j = 0; j <= i;j++)
+		    {
+		        System.out.print(""+count);
+		    }
+		    System.out.println();
+		    count++;
+		}
+		
+	}
+}
