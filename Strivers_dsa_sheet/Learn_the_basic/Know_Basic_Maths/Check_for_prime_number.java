8a9e0e7867fbae6394c9fa44ece1d7460419f55c
Rutuja Upase <rutujaupase13@gmail.com>
2026-07-13 09:40:52 +0530

    add solutions for the maths [roblems

---
+++ Strivers_dsa_sheet/Learn_the_basic/Know_Basic_Maths/Check_for_prime_number.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+public class Main
+{
+	public static void main(String[] args) {
+	    Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the number");
+		int num = sc.nextInt();
+		int count = 0;
+		
+		for(int i = 1;i <= num;i++)
+		{
+		    if(num % i == 0)
+		    {
+		        count++;
+		    }
+		}
+		
+	   if(count == 2) 
+		{
+		    System.out.println(num+" is prime number") ;
+		}
+		
+	else {
+		    System.out.println(num+" is not prime number");
+		}
+		
+	}
+}
